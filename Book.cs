e41c612492bda1a4a4abd2de9823fe5d8d8539c4
vishlesha02 <vishleshamortale18@gmail.com>
2023-07-12 11:56:23 +0530

    Library Management

---
+++ Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement
+{
+    public class Book
+    {
+        
+            public int Book_id;
+
+            public string Title;
+
+            public string Author;
+
+            public string Genre;
+
+            public bool Borrowed;
+
+
+        public Book()
+        { }
+
+        public Book(int bookid, string title, string author, string genre, bool borrowed) 
+        {
+            Book_id=bookid;
+            Title = title;
+            Author= author;
+            Genre= genre;
+            Borrowed= borrowed;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement
+{
+    public class Program
+    {
+        public void Main(String[] args)
+        {
+            Library library = new Library();
+
+        
+        }
+    }
+}
